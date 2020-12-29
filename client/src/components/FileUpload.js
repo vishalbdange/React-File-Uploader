0b3d31c8cc5b59130aa6d3d5e11d9f7c6046f0ad
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2020-12-29 12:44:28 +0530

    Modified and added File Uploader

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
+import Message from './Message';
+import Progress from './Progress';
 import axios from "axios"
 const FileUpload = () => {
 
     const [file,setFile] = useState("");
     const [fileName,setFileName] = useState("Choose File");
-    const [uplLoadedFile,setUploadedFile] = useState({});
+    const [uploadedFile,setUploadedFile] = useState({});
+    const [message, setMessage] = useState('');
+    const [uploadPercentage, setUploadPercentage] = useState(0);
 
        const onChange = (e) => {
            setFile(e.target.files[0]);
@@ -19,9 +23,25 @@ const FileUpload = () => {
              const res = await axios.post("/upload",formData, {
                  headers : {
                      "Content-Type" :  "multipart/form-data"
-                 }
+                 },
+             onUploadProgress : progressEvent => {
+                setUploadPercentage(
+                  parseInt(
+                    Math.round((progressEvent.loaded * 100) / progressEvent.total)
+                  )
+                );
+      
+                // Clear percentage
+                setTimeout(() => setUploadPercentage(0), 10000);
+              }
+             
              });
+
+            
        
+      
+
+
          const {fileName , filePath} = res.data;
           setUploadedFile({fileName , filePath});
 
@@ -39,6 +59,7 @@ const FileUpload = () => {
        }
     return (
         <>
+        {message ? <Message msg={message} /> : null}
             <form onSubmit={onSubmit}>
             <div className="custom-file mb-4">
   <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
@@ -46,12 +67,22 @@ const FileUpload = () => {
       {fileName}
   </label>
 </div>
+
+<Progress percentage={uploadPercentage} />
                 <input
                     type="submit"
                     value="Upload"
                     className="btn btn-primary btn-block mt-4 w-100" />
 
             </form>
+            {uploadedFile ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+          </div>
+        </div>
+      ) : null}
         </>
     )
 }
